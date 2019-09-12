56c3e192be128e35ef9cb82d3c1a9cb6f28ab27d
Prasad <jeetendra.prasad@t-systems.com>
2019-09-12 13:14:25 +0530

    42. Using ngIf to conditionally display.mp4

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -13,11 +13,11 @@ export class AuthService {
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
-          const user = response;
-          if(user) {
-            localStorage.setItem('token', user.token);
-          }
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
         }
+      }
       )
     );
   }
